e30b1bc922b2b586c0bb2b3514a94a21d7ce4028
KozlovskyS <skozmail@gmail.com>
2025-03-16 19:12:05 +0300

    Added DataHelper class

---
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -0,0 +1,135 @@
+package ru.netology.data;
+
+import com.github.javafaker.Faker;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+public class DataHelper {
+
+    private DataHelper() {
+    }
+
+    public static String getApprovedCardNumber() {
+        return new String("1111 2222 3333 4444");
+    }
+
+    public static String getDeclinedCardNumber() {
+        return new String("5555 6666 7777 8888");
+    }
+
+    private static String generateRandomName(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        return faker.name().username();
+    }
+
+    public static String generateValidMonth() {
+        return  LocalDate.now().format(DateTimeFormatter.ofPattern("MM"));
+    }
+
+    public static String generateValidYear() {
+       return  LocalDate.now().format(DateTimeFormatter.ofPattern("yy"));
+    }
+
+    public static String generatePastMonth() { //прошедший месяц
+        return LocalDate.now().minusMonths(1).format(DateTimeFormatter.ofPattern("MM"));
+    }
+
+    public static String generatePastYear() {  //прошедший год
+        return LocalDate.now().minusYears(1).format(DateTimeFormatter.ofPattern("yy"));
+    }
+
+    public static String generateInvalidMonth() {
+        int invalidMonth = new Random().nextInt(87) + 13; // неправильный месяц с 13 по 99
+        return String.valueOf(invalidMonth);
+    }
+
+    public static CvvCode generateRandomCvvCode() {
+        Faker faker = new Faker();
+        return new CvvCode(faker.numerify("###"));
+    }
+
+    public static CvvCode generateInvalidCvvCode() {
+        Faker faker = new Faker();
+        return new CvvCode(faker.numerify("##"));
+    }
+
+    // создание данных для авторизации
+    public static AuthInfo generateValidApprovedUser() {
+        return new AuthInfo(getApprovedCardNumber(), generateValidMonth(), generateValidYear(), generateRandomName("en"), generateRandomCvvCode());
+    }
+
+    public static AuthInfo generateValidDeclinedUser() {
+        return new AuthInfo(getDeclinedCardNumber(), generateValidMonth(), generateValidYear(), generateRandomName("en"), generateRandomCvvCode());
+    }
+
+    public static AuthInfo generateApprovedEmptyCardNumber() {
+        return new AuthInfo("", generateValidMonth(), generateValidYear(), generateRandomName("en"), generateRandomCvvCode());
+    }
+
+    public static AuthInfo generateApprovedEmptyMonth() {
+        return new AuthInfo(getApprovedCardNumber(), "", generateValidYear(), generateRandomName("en"), generateRandomCvvCode());
+    }
+
+    public static AuthInfo generateApprovedEmptyYear() {
+        return new AuthInfo(getApprovedCardNumber(), generateValidMonth(), "", generateRandomName("en"), generateRandomCvvCode());
+    }
+
+    public static AuthInfo generateApprovedEmptyName() {
+        return new AuthInfo(getApprovedCardNumber(), generateValidMonth(), generateValidYear(), "", generateRandomCvvCode());
+    }
+
+    public static AuthInfo generateApprovedEmptyCvv() {
+        return new AuthInfo(getApprovedCardNumber(), generateValidMonth(), generateValidYear(), generateRandomName("en"),null );
+    }
+
+    public static AuthInfo generateApprovedZeroMonth() {
+        return new AuthInfo(getApprovedCardNumber(), "00", generateValidYear(), generateRandomName("en"), generateRandomCvvCode());
+    }
+
+    public static AuthInfo generateApprovedInValidMonth() {
+        return new AuthInfo(getApprovedCardNumber(), generateInvalidMonth(), generateValidYear(), generateRandomName("en"), generateRandomCvvCode());
+    }
+
+    public static AuthInfo generateApprovedPastMonth() {
+        return new AuthInfo(getApprovedCardNumber(), generatePastMonth(), generateValidYear(), generateRandomName("en"), generateRandomCvvCode());
+    }
+
+    public static AuthInfo generateApprovedPastYear() {
+        return new AuthInfo(getApprovedCardNumber(), generateValidMonth(), generatePastYear(), generateRandomName("en"), generateRandomCvvCode());
+    }
+
+    public static AuthInfo generateApprovedCyrillicName() {
+        return new AuthInfo(getApprovedCardNumber(), generateValidMonth(), generateValidYear(), generateRandomName("ru"), generateRandomCvvCode());
+    }
+
+    public static AuthInfo generateApprovedNumberName() {
+        return new AuthInfo(getApprovedCardNumber(), generateValidMonth(), generateValidYear(), "2346", generateRandomCvvCode());
+    }
+
+    public static AuthInfo generateApprovedInValidCvv() {
+        return new AuthInfo(getApprovedCardNumber(), generateValidMonth(), generateValidYear(), generateRandomName("en"), generateInvalidCvvCode());
+    }
+
+    @Value
+    public static class AuthInfo {
+        private String cardNumber;
+        private String month;
+        private String year;
+        private String name;
+        private CvvCode cvvCode;
+    }
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Data
+    public static class CvvCode {
+        String cvvCode;
+    }
+}
